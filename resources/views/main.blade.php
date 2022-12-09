8e1661e1a48916f97e34ff0c7578629b36adccdc
blizzend <xcrobertasxc@gmail.com>
2022-12-09 13:48:57 +0200

    first commit

---
+++ resources/views/main.blade.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
+  <body>
+    
+  
+  
+   <div class="container">
+   
+
+   <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Shop</a>
+
+    <button type="submit" class="btn btn-primary">Register</button>
+    <button type="submit" class="btn btn-primary">Prisijunkti</button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+      
+      </div>
+    </div>
+  </div>
+</nav>
+   
+<div class="shopping-cart">
+
+
+
+
+  <div class="product">
+    <div class="product-image">
+      <img src="longboard-as-tempish-fox-n.jpg">
+    </div>
+    <div class="product-details">
+      <div class="product-title">Longboard'as Tempish FOX-N</div>
+     
+    </div>
+    <div class="product-price">79,00€</div>
+    <div class="product-quantity">
+      <input type="number" value="1" min="1">
+      
+    </div>
+    <button type="submit" class="btn btn-primary">plačiau</button>
+    <div class="product-removal">
+      
+    </div>
+    <div class="product-line-price"></div>
+  </div>
+
+  <div class="product">
+    <div class="product-image">
+      <img src="riedlente-tempish-free-spirit.jpg">
+    </div>
+    <div class="product-details">
+      <div class="product-title">Riedlente TEMPISH free Spirit</div>
+      
+    </div>
+    <div class="product-price">79,20€</div>
+    <div class="product-quantity">
+      <input type="number" value="2" min="1">
+    </div>
+    <button type="submit" class="btn btn-primary">plačiau</button>
+    <div class="product-removal">
+      
+    </div>
+    <div class="product">
+    <div class="product-image">
+      <img src="riedlente-tempish-ontop.jpg">
+    </div>
+    <div class="product-details">
+      <div class="product-title">Riedlente TEMPISH Ontop</div>
+      
+    </div>
+    <div class="product-price">79,20€</div>
+    <div class="product-quantity">
+      <input type="number" value="3" min="1">
+    </div>
+    <button type="submit" class="btn btn-primary">plačiau</button>
+    </div>
+
+    <div class="product">
+    <div class="product-image">
+      <img src="riedlente-tempish-blue-wolf.jpg">
+    </div>
+    <div class="product-details">
+      <div class="product-title">Riedlente TEMPISH BLUE WOLF</div>
+      
+    </div>
+    <div class="product-price">71,20€</div>
+    <div class="product-quantity">
+      <input type="number" value="4" min="1">
+    </div>
+    <button type="submit" class="btn btn-primary">plačiau</button>
+  </div>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
